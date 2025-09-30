e4aaeebf37b76ae2bf717bc413aad73b406f9f04
unknown <nariga.c@gmail.com>
2025-09-30 21:21:02 +0100

    fix backend state

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -2,8 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "terraform-state-bucket-nrcit-464037860553"
     key            = "redshift/terraform.tfstate"
-    region         = "us-east-1"
-#    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
+    region         = "eu-west-2"
+ }
 }
