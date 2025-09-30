87294b98cc86523552daa0d86ef8f73083e7ee97
unknown <nariga.c@gmail.com>
2025-09-30 21:46:05 +0100

    cleaning code

--- infra/terraform/modules/redshift-cluster/main.tf
+++ infra/terraform/modules/redshift-cluster/main.tf
@@ -1,12 +1,12 @@
-resource "aws_redshift_cluster" "this" {
-  cluster_identifier = var.cluster_identifier
-  node_type          = var.node_type
-  master_username    = var.master_username
-  master_password    = random_password.password.result
-  database_name      = var.database_name
-  cluster_type       = var.cluster_type
-  number_of_nodes    = var.number_of_nodes
-}
+# resource "aws_redshift_cluster" "this" {
+#   cluster_identifier = var.cluster_identifier
+#   node_type          = var.node_type
+#   master_username    = var.master_username
+#   master_password    = random_password.password.result
+#   database_name      = var.database_name
+#   cluster_type       = var.cluster_type
+#   number_of_nodes    = var.number_of_nodes
+# }
 
 resource "random_password" "password" {
   length           = 16
